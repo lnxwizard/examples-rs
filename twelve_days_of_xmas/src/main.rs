95be380b84e24510bfeec3c41572e432eed2a157
lnxwizard <alperakca79@outlook.com>
2023-08-31 15:14:56 +0300

    Added new example project: Twelve Days of XMas

---
+++ twelve_days_of_xmas/src/main.rs
@@ -0,0 +1,37 @@
+fn main() {
+    let days: [&str; 12] = [
+        "a partridge in a pear tree",
+        "two turtle doves",
+        "three french hens",
+        "four calling birds",
+        "FIVE GOLD RINGS",
+        "six geese a-laying",
+        "seven swans a-swimming",
+        "eight maids a-milking",
+        "nine ladies dancing",
+        "ten lords a-leaping",
+        "eleven pipers piping",
+        "twelve drummers drumming",
+    ];
+
+    for day in 0..12 {
+        let suffix: &str = match day + 1 {
+            1 => "st",
+            2 => "nd",
+            3 => "rd",
+            _ => "th",
+        };
+
+        println!(
+            "\nOn the {}{} day of xmas my true love gave to me:",
+            day + 1,
+            suffix
+        );
+        for item in (0..day + 1).rev() {
+            if day > 0 && item == 0 {
+                print!("And ");
+            }
+            println!("{}", days[item]);
+        }
+    }
+}
